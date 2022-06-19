c0124b280dde52ff0d810d50398a04c72bdc7faf
Ben Kerllenevich <ben@omega24.dev>
2022-06-19 19:37:47 -0400

    icons in jsx

--- app/routes/__layout/experience.tsx
+++ app/routes/__layout/experience.tsx
@@ -1,6 +1,13 @@
 import HorizontalSection from "~/components/sections/HorizontalSection";
 import Section from "~/components/sections/Section";
 import IconDescription from "~/components/IconDescription";
+import PurpurIcon from "~/components/icons/PurpurIcon";
+import VueIcon from "~/components/icons/VueIcon";
+import ReactIcon from "~/components/icons/ReactIcon";
+import RustIcon from "~/components/icons/RustIcon";
+import GoIcon from "~/components/icons/GoIcon";
+import TSIcon from "~/components/icons/TSIcon";
+import JavaIcon from "~/components/icons/JavaIcon";
 
 export default function Experience() {
     return (
@@ -14,31 +21,37 @@ export default function Experience() {
                     <IconDescription
                         name="Java"
                         description="I am highly fluent in Java and use it for anything Minecraft or Discord related."
+                        icon={<JavaIcon />}
                     />
 
                     <IconDescription
                         name="TypeScript"
                         description="I am highly experienced in TypeScript and use it for anything web related."
+                        icon={<TSIcon />}
                     />
 
                     <IconDescription
                         name="Go"
                         description="I am fluent in Go and use it whenever simplicity and speed is a concern."
+                        icon={<GoIcon />}
                     />
 
                     <IconDescription
                         name="Rust"
                         description="I am proficient in Rust and use it for everyday tasks where performance is important."
+                        icon={<RustIcon />}
                     />
 
                     <IconDescription
                         name="React"
                         description="I am very experienced in React and use it as my go-to choice of framework."
+                        icon={<ReactIcon />}
                     />
 
                     <IconDescription
                         name="Vue"
                         description="I am proficient in Vue and use it for any projects not using React."
+                        icon={<VueIcon />}
                         isLast={true}
                     />
                 </div>
@@ -53,6 +66,7 @@ export default function Experience() {
                     <IconDescription
                         name="Purpur"
                         description="Purpur is a Minecraft server JAR designed for configurability and fun, exciting gameplay features."
+                        icon={<PurpurIcon />}
                         link="https://github.com/PurpurMC/Purpur"
                         isLast={true}
                     />
